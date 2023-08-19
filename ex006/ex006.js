5595f3b841c6a48d523f8a987ef31847684dc47b
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-08-19 02:46:33 -0300

    ex06

--- ex006/ex006.js
+++ ex006/ex006.js
@@ -1,11 +1,17 @@
-function inverter() {
-    let n = document.querySelector('#num')
-    let resposta = document.querySelector('#res')
-    let numero = Number(n.value)
-    let numinv = numero.split("").reverse().join("")
-    
-    resposta.innerHTML += numinv
-    
+let n = document.querySelector('.num')
+let resposta = document.querySelector('.res')
+let botao = document.querySelector('.invertendo')
+
+botao.addEventListener('click', function(e){
+    inverteNumero()
+})
+
+function inverteNumero() {
+    return num.replace()
 }
+
+inverteNumero()
+    
+
 
    
